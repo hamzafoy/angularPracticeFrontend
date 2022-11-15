0b685c35f848ab75434c8fd75eff4bfa22c0f931
hamzafoy <hamzaoflouisville@gmail.com>
2022-11-15 14:08:17 -0500

    working with input properties by creating default ratings upon load.

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-rating',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RatingComponent implements OnInit {
 
-  rating: number = 0;
+  @Input() rating: number = 0;
 
   onClick(ratingValue: number) {
     this.rating = ratingValue;
